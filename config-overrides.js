7df291f17b0b2fb7757c4a6d2f5f30f4ddb7ce4f
zakaribel-devzakaribel@hotmail.com <zakaribel@hotmail.com>
2024-01-12 10:12:27 +0100

    webpack

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,9 @@
 module.exports = function override(config, env) {
-    // Ajouter un fallback pour 'crypto'
-    config.resolve.fallback = {
-        ...config.resolve.fallback, // pour garder les fallbacks existants
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
 
+    config.resolve.fallback = {
+        ...config.resolve.fallback, // pour garder les fallbacks existants qui sont surement (j'en suis sur à quasi 100% ) inclus dans create-react-app
+        crypto: require.resolve('crypto-browserify'), // jaurai plus les warning liés à bcrypt
+        stream: require.resolve('stream-browserify'), //...
     };
     return config;
 };
